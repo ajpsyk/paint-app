6843efa50b24bf3a2ff78f350536096c270af98b
Anthony Psyk <ajpsyk@gmail.com>
2022-01-15 17:51:42 -0800

    Added project name component

---
+++ app/components/Name.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Name() {
+  const [name, setName] = useState('')
+  return (
+    <label className="header-name">
+      <input
+        value={}
+        onChange={}
+        onClick={}
+        placeholder="Untitled"
+      />
+    </label>
+  )
+}
