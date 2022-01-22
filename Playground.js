83489e7250b2f83e4b3699f0638e28a7f7f37b99
Anthony Psyk <ajpsyk@gmail.com>
2022-01-22 15:44:06 -0800

    useEffect example

--- Playground.js
+++ Playground.js
@@ -3,8 +3,14 @@ import randomColor from 'randomcolor'
 
 export default function Playground() {
   const [count, setCount] = useState(0)
+
+  const [color, setColor] = useState(null)
+  useEffect(() => {
+    setColor(randomColor())
+  }, [count])
+
   return (
-    <div>
+    <div style={{ borderTop: `10px solid ${color}`}}>
       {count}
       <button onClick={() => setCount(count - 1)}>-</button>
       <button onClick={() => setCount(count + 1)}>+</button>
